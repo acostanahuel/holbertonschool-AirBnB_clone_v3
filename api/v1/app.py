d46eb85320585a19a237c49f8aa03a0ef52bbb91
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-21 15:20:01 -0300

    comments

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,12 @@
 Flask API
 """
 from flask import Flask, render_template
-app = Flask(__name__)
 from models import storage
 from api.v1.views import app_views
+app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def app_close(exception=None):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+Flask Blueprint
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
+    """ status endopint """
     return jsonify({"status": "OK"})
