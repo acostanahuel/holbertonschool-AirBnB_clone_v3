76730b025d04693a2e8ff20e34571253f3695300
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-21 16:41:20 -0300

    states post fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,15 +8,27 @@ from models import storage
 from models.state import State
 
 
-@app_views.routes('/states', methods=['GET'], strict_slashes=False)
+@app_views.routes('/states', methods=['GET', 'POST'], strict_slashes=False)
 def get_states():
     """list all states"""
-    states = storage.all(State).values()
-    state_list = [state.to_dict() for state in states]
-    return jsonify(state_list)
+    if request.method == 'GET':
+        states = storage.all(State).values()
+        state_list = [state.to_dict() for state in states]
+        return jsonify(state_list)
+
+    if request.method == 'POST':
+        new = request.get_json()
+        if not new:
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
+        if "name" not in new.keys():
+            return make_response(jsonify({"error": "Missing name"}), 400)
+        inst = State(**new)
+        state = storage.new(inst)
+        storage.save()
+        return make_response(jsonify(inst.to_dict()), 201)
 
 
-@app_views.routes('/states/<str:state_id>', methods=['GET', 'DELETE', 'POST', 'PUT'], strict_slashes=False)
+@app_views.routes('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
 def get_states(state_id):
     """get create delete update any states"""
     state = storage.get(State, state_id)
@@ -32,17 +44,6 @@ def get_states(state_id):
         storage.delete(state)
         storage.save()
         return jsonify({"status code": 200})
-
-    if request.method == 'POST':
-        new = request.get_json()
-        if not new:
-            return make_response(jsonify({"error": "Not a JSON"}), 400)
-        if "name" not in new.keys():
-            return make_response(jsonify({"error": "Missing name"}), 400)
-        inst = State(**new)
-        state = storage.new(inst)
-        storage.save()
-        return make_response(jsonify(inst.to_dict()), 201)
 
     if request.method == 'PUT':
         state = storage.get(State, state_id)
