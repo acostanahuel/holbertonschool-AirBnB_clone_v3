073fc3063e02d0f48660cc7659cb8ca243f06475
jotalgs <jose.pgilsuarez@gmail.com>
2024-04-21 15:41:38 -0300

    stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Flask API
 """
 from flask import Flask, render_template
 from models import storage
-from views import app_views
+from api.v1.views import app_views
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """
 Flask Blueprint
 """
-from views import app_views
+from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 from models.amenity import Amenity
