b724c6f97a53ba1b739952dfd3416b6544eea1f4
jotalgs <jose.pgilsuarez@gmail.com>
2024-07-10 14:36:19 +0000

    404 api.v1.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 Flask states Blueprint
 """
-from views import app_views
+from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
